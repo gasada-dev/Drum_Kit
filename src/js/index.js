311526cccfdeeab2a5600bf073d2f2bb28cc5cb2
gasada <gasada@protonmail.com>
2021-03-24 21:12:36 +0300

    feat: add event keydown and change background

---
+++ src/js/index.js
@@ -0,0 +1,20 @@
+
+const removeTransition = (e) => {
+  if (e.propertyName !== 'transform') return;
+  e.target.classList.remove('playing');
+}
+
+const playSound = (e) => {
+  const audio = document.querySelector(`audio[data-key="${e.code}"]`);
+  const key = document.querySelector(`div[data-key="${e.code}"]`);
+  if (!audio) return;
+
+  key.classList.add('playing');
+  audio.currentTime = 0;
+  audio.play();
+}
+
+
+const keys = Array.from(document.querySelectorAll('.key'));
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+document.addEventListener('keydown', playSound);
