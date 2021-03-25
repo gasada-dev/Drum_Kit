e3311d12c8da53d2aa31b6ae462a583807f3e586
gasada <gasada@protonmail.com>
2021-03-25 15:51:50 +0300

    refactor: add space in style.scss and add funtion but not add new duntionality

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,3 @@
-
 const removeTransition = (e) => {
   if (e.propertyName !== 'transform') return;
   e.target.classList.remove('playing');
@@ -7,14 +6,18 @@ const removeTransition = (e) => {
 const playSound = (e) => {
   const audio = document.querySelector(`audio[data-key="${e.code}"]`);
   const key = document.querySelector(`div[data-key="${e.code}"]`);
-  if (!audio) return;
 
   key.classList.add('playing');
   audio.currentTime = 0;
   audio.play();
 }
 
+const play = () => {
+  const keys = Array.from(document.querySelectorAll('.key'));
+  keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+  document.addEventListener('keydown', playSound);
+}
 
-const keys = Array.from(document.querySelectorAll('.key'));
-keys.forEach(key => key.addEventListener('transitionend', removeTransition));
-document.addEventListener('keydown', playSound);
+play()
+
+
